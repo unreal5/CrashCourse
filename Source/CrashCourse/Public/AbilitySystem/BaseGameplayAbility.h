aece498bcffdacbe4b5978763818559bdcb0988f
lbjtj <lbjtj@outlook.com>
2026-03-16 17:05:59 +0800

    0017 base ability

---
+++ Source/CrashCourse/Public/AbilitySystem/BaseGameplayAbility.h
@@ -0,0 +1,20 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BaseGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRASHCOURSE_API UBaseGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Crash|Debug")
+	bool bDrawDebug = false;
+};
