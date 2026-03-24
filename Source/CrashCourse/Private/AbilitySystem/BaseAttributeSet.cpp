bb4106ce927e1a1bf0882aaeacb985d72dc2b03c
lbjtj <lbjtj@outlook.com>
2026-03-24 17:17:27 +0800

    0031 attribute set

---
+++ Source/CrashCourse/Private/AbilitySystem/BaseAttributeSet.cpp
@@ -0,0 +1,36 @@
+﻿// 版权没有，拷贝自由。
+
+
+#include "AbilitySystem/BaseAttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Health, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, MaxHealth, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Mana, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, MaxMana, COND_None, REPNOTIFY_Always)
+}
+
+void UBaseAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Health, OldValue)
+}
+
+void UBaseAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, MaxHealth, OldValue)
+}
+
+void UBaseAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Mana, OldValue)
+}
+
+void UBaseAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, MaxMana, OldValue)
+}
---
+++ Source/CrashCourse/Public/AbilitySystem/BaseAttributeSet.h
@@ -0,0 +1,49 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "BaseAttributeSet.generated.h"
+
+
+UCLASS()
+class CRASHCOURSE_API UBaseAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category="Crash|Attributes")
+	FGameplayAttributeData Health;
+
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category="Crash|Attributes")
+	FGameplayAttributeData MaxHealth;
+
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Mana, Category="Crash|Attributes")
+	FGameplayAttributeData Mana;
+
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxMana, Category="Crash|Attributes")
+	FGameplayAttributeData MaxMana;
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, Health)
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, MaxHealth)
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, Mana)
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, MaxMana)
+
+private:
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue);
+};
