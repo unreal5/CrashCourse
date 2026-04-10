553bfb609698bcf9c14753752264952d1b98179b
lbjtj <lbjtj@outlook.com>
2026-04-10 16:13:37 +0800

    0044 attribute change async task

---
+++ Source/CrashCourse/Private/Tasks/BaseAttributeChangeTask.cpp
@@ -0,0 +1,38 @@
+﻿// 版权没有，拷贝自由。
+
+
+#include "Tasks/BaseAttributeChangeTask.h"
+
+#include "AbilitySystemComponent.h"
+
+UBaseAttributeChangeTask* UBaseAttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* InASC,
+                                                                             FGameplayAttribute InAttribute)
+{
+	if (!InASC) return nullptr;
+
+	auto WaitForAttributeChangedTask = NewObject<ThisClass>();
+	if (!WaitForAttributeChangedTask) return nullptr;
+	WaitForAttributeChangedTask->ASC = InASC;
+	WaitForAttributeChangedTask->AttributeToListenFor = InAttribute;
+
+	InASC->GetGameplayAttributeValueChangeDelegate(InAttribute).AddUObject(
+		WaitForAttributeChangedTask, &UBaseAttributeChangeTask::AttributeChanged);
+	
+	return WaitForAttributeChangedTask;
+}
+
+void UBaseAttributeChangeTask::EndTask()
+{
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	}
+	// MarkPendingKill() will mark the object for garbage collection, but it won't be collected until the next GC cycle. This is useful for cleaning up objects that are no longer needed, but it allows any pending operations to complete before the object is destroyed. In this case, it ensures that any pending attribute change notifications are processed before the task is destroyed.
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UBaseAttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data) const
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
---
+++ Source/CrashCourse/Public/Tasks/BaseAttributeChangeTask.h
@@ -0,0 +1,32 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "BaseAttributeChangeTask.generated.h"
+
+struct FOnAttributeChangeData;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAttributeChangeSignature, FGameplayAttribute, Attribute, float,
+                                               NewValue, float, OldValue);
+
+UCLASS()
+class CRASHCOURSE_API UBaseAttributeChangeTask : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangeSignature OnAttributeChanged;
+	
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UBaseAttributeChangeTask* ListenForAttributeChange(UAbilitySystemComponent* InASC, FGameplayAttribute InAttribute);
+	
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+	
+	void AttributeChanged(const FOnAttributeChangeData& Data) const;
+private:
+	TWeakObjectPtr<UAbilitySystemComponent> ASC;
+	FGameplayAttribute AttributeToListenFor;
+};
